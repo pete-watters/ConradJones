54f0a8f9170e58103c360e9538beed978e1f138d
Peter Watters <peter.j.watters@gmail.com>
2015-11-04 18:32:00 +0000

    Final updates

--- wp-content/themes/conradjones_cuilduin/page-site-plan.php
+++ wp-content/themes/conradjones_cuilduin/page-site-plan.php
@@ -1,4 +1,4 @@
-hea<?php
+<?php
 /**
  * conradjones_cuilduin template for displaying Pages
  *
@@ -16,9 +16,7 @@ get_header(); ?>
 
 				<h3 class="sitemap-legend-heading">Click house type to view floor plan</h3>
 					<div class="sitemap-legend">
-						<?xml version="1.0" encoding="utf-8"?>
-						<!-- Generator: Adobe Illustrator 16.0.4, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-						<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+
 						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 							 width="416.656px" height="151px" viewBox="793.443 -791.715 416.656 151" enable-background="new 793.443 -791.715 416.656 151"
 							 xml:space="preserve">
